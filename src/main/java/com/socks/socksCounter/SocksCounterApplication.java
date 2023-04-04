07ac11896af30908a0bcbf1a8d31ebea27b7dae0
Gleb <gleb.alef@gmail.com>
2023-04-04 10:38:31 +0100

    Initial commit

---
+++ src/main/java/com/socks/socksCounter/SocksCounterApplication.java
@@ -0,0 +1,13 @@
+package com.socks.socksCounter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocksCounterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocksCounterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/socks/socksCounter/SocksCounterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socks.socksCounter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocksCounterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
